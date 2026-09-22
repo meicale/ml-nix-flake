e0200831e4018a76332683019a8cd9bee5e4f6ba
me <1@222.com>
2026-09-23 06:57:38 +0800

    2

--- flake.nix
+++ flake.nix
@@ -1,12 +1,12 @@
 {
   description = "dev shell with uv and cuda out of the box";
 
-  nixConfig = {
-    extra-substituters = [ "https://cuda-maintainers.cachix.org" ];
-    extra-trusted-public-keys = [
-      "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-    ];
-  };
+  # nixConfig = {
+    # extra-substituters = [ "https://cuda-maintainers.cachix.org" ];
+    # extra-trusted-public-keys = [
+      # "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
+    # ];
+  # };
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
